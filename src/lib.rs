39843dc122a05fd667f1868c6a34bf11762ce9dc
Ivan Boldyrev <lispnik@gmail.com>
2020-05-20 21:27:41 +0300

    Fixes for noop malloc
    1. Set errno in malloc.
    2. Check for null pointer in free.  Even if it does nothing
       anyway.

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,22 @@ use libc;
 // A minimal malloc implementation that simply returns null pointer.
 #[no_mangle]
 pub extern  fn malloc(_size: libc::size_t) -> *mut ffi::c_void {
+    unsafe {
+        // Glibc expects malloc to set errno on any failure
+        // (i.e. when null pointer is returned).  Reference:
+        // https://linux.die.net/man/3/malloc, Notes section.
+        *libc::__errno_location() = libc::ENOMEM;
+    }
     ptr::null_mut()
 }
 
 // A minimal free implementation that does nothing.
 #[no_mangle]
-pub extern fn free(_ptr: *mut ffi::c_void) {
+pub extern fn free(p: *mut ffi::c_void) {
+    // free has to return immediately if p in null.
+    if p == ptr::null_mut() {
+        return;
+    }
 }
 
 // An obligatory no_std panic_handler.
